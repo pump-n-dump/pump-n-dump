d94be89f86618bae27645606c1e1f03d04ffeeb4
Hannan Rhodes <hannan@rokk3rlabs.com>
2018-03-20 14:57:05 -0400

    added the login fucntion and file system

---
+++ public/scripts/binance.js
@@ -0,0 +1,12 @@
+const binance = require('node-binance-api')
+
+function init(key, secret){
+
+    binance.options({
+        APIKEY: key,
+        APISECRET: secret,
+        useServerTime: true, // If you get timestamp errors, synchronize to server time at startup
+        test: true // If you want to use sandbox mode where orders are simulated
+      })
+      console.log("ayy you in boi");
+}
